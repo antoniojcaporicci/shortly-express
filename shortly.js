96df009690ca6049f2bcc1af52ab5c63c81b33ca
Hack Reactor Student <student@hackreactor.com>
2017-09-14 20:39:02 -0500

    password hash compare functional

--- shortly.js
+++ shortly.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-app.use(session({secret: 'cats', resave: false, saveUninitialized: true}))
+app.use(session({secret: 'cats', resave: false, saveUninitialized: true}));
 
 
 app.get('/', isLoggedIn, function(req, res) {
